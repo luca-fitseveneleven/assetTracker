5f60ffc8d103c730a8f812cd7f6582155f6401e6
LucaGerlich <lucagerlich@gmail.com>
2026-03-30 14:32:41 +0200

    fix: use env variables for Sentry org, project, and URL

--- next.config.mjs
+++ next.config.mjs
@@ -70,9 +70,11 @@ export default withBundleAnalyzer(withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "fitseveneleven",
+  org: process.env.SENTRY_ORG,
 
-  project: "javascript-nextjs",
+  project: process.env.SENTRY_PROJECT,
+
+  url: process.env.SENTRY_URL,
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
